482145ff4a4dd8a181637cc8d2e34b9d9093f9d9
Upasana Devkota <devkotaupasana@gmail.com>
2023-11-16 00:04:10 -0500

    WIP Added style and dataset code.

--- main.js
+++ main.js
@@ -24,38 +24,46 @@ const itunesApp = {
           const entryPoint = document.getElementById('container')
           console.log(data)
           console.log(data.results)
-
-          function addBtnEventListener(){
-            let buttonsPlay = document.querySelectorAll(".playBtn")
-            for(let button of buttonsPlay){
-              button.addEventListener( 'click', (event) =>{
-                event.preventDefault()
-                addAudioSource(button.dataset.id)
-                //add addAudioSource() func
-              })
-            }
-          }
           
 
           function dataToHTML (song){
-//dataset.id 
-            addBtnEventListener()
-            return `<div id="songDiv" class="songDivs">
+            return `<div class="songDiv" class="songDivs">
                   <img src="${song.artworkUrl100}" alt="Song Picture"/>
-                  <h3 id="songTitleData">${song.trackName}</h3>
-                  <h4 id="artistAlbumData">${song.collectionName}</h4>
-                  <h2 id="artistNameData">${song.artistName}</h2>
-                  <button class="playBtn" data-id=${song.trackId} type="button">Play Now</button>
-                  <audio src="${song.previewUrl}" controls></audio>
+                  <h2 class="songTitleData">${song.trackName}</h2>
+                  <h3 class="artistNameData">${song.artistName}</h3>
+                  <h4 class="artistAlbumData">${song.collectionName}</h4>
+                  <button class="playBtn" data-id="${song.trackId}" data-audio="${song.previewUrl}" data-artist="${song.artistName}" data-song="${song.trackName}"type="button">Play Now</button>
                 </div>`
           }
-          let allSongs = data.results.map((song) => dataToHTML(song)).join('\n');
+          
+          let allSongs = data.results.map((song) => dataToHTML(song)).join('\n')
 
           entryPoint.innerHTML = allSongs
-        }
-      })
+
+          // this.addAudioSource()
+      } 
+    })
       
   },
+  // addAudioSource: function () {
+  //   let buttonsPlay = document.querySelectorAll(".playBtn")
+  //   for (let button of buttonsPlay) {
+  //     button.addEventListener('click', (event) => {
+  //       event.preventDefault()
+  //       let audioSrc = event.target.dataset.audio
+  //       console.log(audioSrc)
+  //       //uncaught in promise type errorthis.addAudioSource is not a function 
+     
+  //       //if my code did work lol 
+  //       //document.querySelector('figure').classList.remove('hidden')
+  //       // document.querySelector('audio').src = audioSrc
+  //       // let artName = event.target.dataset.artist
+  //       // let musicName = event.target.dataset.trackName
+  //       // document.querySelector('figcaption').innerText = `Now Playing: ${artName} - ${musicName}`
+
+  //     })
+  //   }
+  // },
   addEventListener: function () {
     const form = document.getElementById('artistSearch')
     form.addEventListener('submit', this.getSongs) 
@@ -66,10 +74,24 @@ const itunesApp = {
 
   itunesApp.addEventListener()
 
+//Add the event listeners to the button
+//using the unique data-id on each playBtn, send the audio src (song.previewUrl) to the addAudioSrc()
+//function addAudioSrc()
+
+
   //WIP 
-  //display all songs
   //audio
   //css 
   //refactor into OOP so that each function only perfoms one action
 
  
+//code that may or may not work but not using this atm
+// function addBtnEventListener(){
+//   let buttonsPlay = document.querySelectorAll(".playBtn")
+//   for(let button of buttonsPlay){
+//     button.addEventListener( 'click', (event) =>{
+//       event.preventDefault()
+//       addAudioSource(button.dataset.id)
+//     })
+//   }
+// }
